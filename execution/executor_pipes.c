805d190f1f4c7601698b8e315675fd3db80791c9
Joao Victor Brol Carneiro <jbrol-ca@c2r5s7.42porto.com>
2025-04-14 21:13:12 +0100

    norminetted: executor_pipes.c

--- execution/executor_pipes.c
+++ execution/executor_pipes.c
@@ -1,54 +1,103 @@
 /* ************************************************************************** */
-/* executor_pipes.c                                                         */
+/* */
+/* :::      ::::::::   */
+/* executor_pipes.c                                     :+:      :+:    :+:   */
+/* +:+ +:+         +:+     */
+/* By: hde-barr <hde-barr@student.42.fr>          +#+  +:+       +#+        */
+/* mplíčdeps   +#+           */
+/* Created: 2025/04/14 00:00:00 by hde-barr          #+#    #+#             */
+/* Updated: 2025/04/14 21:10:00 by hde-barr         ###   ########.fr       */
+/* */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-** setup_pipes:
-**   - Creates a pipe and returns the result.
-*/
-int setup_pipes(int pipefd[2])
+/* Creates a pipe */
+int	setup_pipes(int pipefd[2])
 {
-    return (pipe(pipefd));
+	return (pipe(pipefd));
 }
 
-/*
-** execute_pipe_command:
-**   - Executes a command pipeline.
-**   - Forks one child for the left side (writing end) and one for the right side (reading end).
-*/
-int execute_pipe_command(t_shell *shell, t_node_tree *node)
+/* Waits for pipe children and determines final exit code from last command */
+static int	wait_and_get_exit_code(pid_t pid1, pid_t pid2)
 {
-    int     pipefd[2];
-    pid_t   pid1 = -1, pid2 = -1;
-    int     status1, status2; // Raw waitpid status
-    int     exit_code = 1;    // Final 0-255 code
+	int	status1;
+	int	status2;
+	int	exit_code;
 
-    if (!node || !node->left || !node->right) { /* Handle error */ return 2; }
-    if (setup_pipes(pipefd) < 0) { /* Handle error */ return 1; }
+	exit_code = 1;
+	waitpid(pid1, &status1, 0);
+	waitpid(pid2, &status2, 0);
+	if (WIFEXITED(status2))
+	{
+		exit_code = WEXITSTATUS(status2);
+	}
+	else if (WIFSIGNALED(status2))
+	{
+		exit_code = 128 + WTERMSIG(status2);
+	}
+	else
+	{
+		exit_code = EXIT_FAILURE;
+	}
+	return (exit_code);
+}
 
-    pid1 = fork_left(shell, node, pipefd);
-    if (pid1 < 0) { /* Handle error */ close(pipefd[0]); close(pipefd[1]); return 1; }
+/* Forks left child, checks error, closes parent pipe ends on error */
+static pid_t	fork_left_checked(t_shell *shell, t_node_tree *node, \
+														int pipefd[2])
+{
+	pid_t	pid;
 
-    pid2 = fork_right(shell, node, pipefd);
-    if (pid2 < 0) { /* Handle error */ close(pipefd[0]); close(pipefd[1]); waitpid(pid1, &status1, 0); return 1; }
+	pid = fork_left(shell, node, pipefd);
+	if (pid < 0)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+	return (pid);
+}
 
-    close(pipefd[0]); close(pipefd[1]); // Parent closes pipe
+/* Forks right child, checks error, closes parent pipe ends on error */
+static pid_t	fork_right_checked(t_shell *shell, t_node_tree *node, \
+														int pipefd[2])
+{
+	pid_t	pid;
 
-    waitpid(pid1, &status1, 0);
-    waitpid(pid2, &status2, 0); // Wait for LAST command
+	pid = fork_right(shell, node, pipefd);
+	if (pid < 0)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+	return (pid);
+}
 
-    // Determine exit code from the LAST command's raw status (status2)
-    if (WIFEXITED(status2)) {
-        exit_code = WEXITSTATUS(status2);
-    } else if (WIFSIGNALED(status2)) {
-        exit_code = 128 + WTERMSIG(status2);
-        // Signal printing is handled by waitpid in execute_external_command
-    } else {
-        exit_code = EXIT_FAILURE;
-    }
+/* Executes a command pipeline (Norminette Compliant) */
+int	execute_pipe_command(t_shell *shell, t_node_tree *node)
+{
+	int		pipefd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+	int		exit_code;
 
-    // execute_ast (the caller) will update g_exit_code
-    return (exit_code); // Return interpreted code
+	pid1 = -1;
+	pid2 = -1;
+	if (!node || !node->left || !node->right)
+		return (2);
+	if (setup_pipes(pipefd) < 0)
+		return (perror("minishell: pipe"), 1);
+	pid1 = fork_left_checked(shell, node, pipefd);
+	if (pid1 < 0)
+		return (1);
+	pid2 = fork_right_checked(shell, node, pipefd);
+	if (pid2 < 0)
+	{
+		waitpid(pid1, NULL, 0);
+		return (1);
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	exit_code = wait_and_get_exit_code(pid1, pid2);
+	return (exit_code);
 }
